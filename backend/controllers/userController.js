7596871033adc3425d2f86b87cb18273b0c5bd20
helen tan <helentanmz0909@gmail.com>
2022-08-22 16:09:31 +0800

    Register User (create new user details in mongoDB)

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,13 @@
 const asyncHandler = require('express-async-handler')
+const bcrypt = require('bcryptjs') // to hash the passwords (can't store plain text pws in database)
+
+const User = require('../models/userModel')
 
 // @desc    Register a new user
 // @route   /api/users
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
-  const {name, email, password} = req.body
+  const { name, email, password } = req.body
 
   // Validation
   if (!name || !email || !password) {
@@ -12,7 +15,35 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error('Please include all fields')
   }
 
-  res.send('Register Route')
+  // Find if user already exists
+  const userExists = await User.findOne({ email })
+
+  if(userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  // Hash the password
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  // Create User
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword
+  })
+
+  if(user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data')
+  }
 })
 
 // @desc    Login a user
