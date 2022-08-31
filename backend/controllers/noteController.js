0e6e299598aa15d5c8f664b45a723075b71b8e25
helen tan <helentanmz0909@gmail.com>
2022-08-31 15:33:02 +0800

    Add notes backend

--- backend/controllers/noteController.js
+++ backend/controllers/noteController.js
@@ -28,7 +28,37 @@ const getNotes = asyncHandler(async (req, res) => {
   res.status(200).json(notes)
 })
 
+// @desc    Create a note for a ticket
+// @route   POST /api/tickets/:ticketId/notes
+// @access  Private
+const addNote = asyncHandler(async (req, res) => {
+  // Get user using the id in the JWT
+  const user = await User.findById(req.user.id)
+
+  if(!user){
+    res.status(401)
+    throw new Error('Úser not found')
+  }
+
+  const ticket = await Ticket.findById(req.params.ticketId)
+
+  if(ticket.user.toString() !== req.user.id) {
+    res.status(401)
+    throw new Error('User not authorized')
+  }
+
+  const note = await Note.create({
+    text: req.body.text,
+    isStaff: false,
+    ticket: req.params.ticketId,
+    user: req.user.id
+  })
+
+  res.status(200).json(note)
+})
+
 
 module.exports = {
-  getNotes
+  getNotes,
+  addNote
 }
