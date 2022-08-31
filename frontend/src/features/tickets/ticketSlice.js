4766b1bba22f3782de6fc481d428e9acef010d33
helen tan <helentanmz0909@gmail.com>
2022-08-31 14:31:28 +0800

    Get single ticket from Redux

--- frontend/src/features/tickets/ticketSlice.js
+++ frontend/src/features/tickets/ticketSlice.js
@@ -34,6 +34,18 @@ export const getTickets = createAsyncThunk('tickets/getAll', async (_, thunkAPI)
   }
 })
 
+// Get single user ticket
+export const getTicket = createAsyncThunk('tickets/get', async (ticketId, thunkAPI) => {
+  try {
+    const token = thunkAPI.getState().auth.user.token
+    return await ticketService.getTicket(ticketId, token)
+  } catch (error) {
+    const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+
+    return thunkAPI.rejectWithValue(message)
+  }
+})
+
 export const ticketSlice = createSlice({
   name: 'ticket',
   initialState,
